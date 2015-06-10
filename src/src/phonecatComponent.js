c657b2aff05df5548cc0e2c6fd53256b81b4a204
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-06-10 21:53:29 +0300

    change phonecatComponent to ES6 syntax

--- src/src/phonecatComponent.js
+++ src/src/phonecatComponent.js
@@ -1,26 +1,20 @@
 'use strict';
 
-angular.module('phonecatApp')
-  .directive('phonecatComponent', phonecatComponent);
+class PhonecatComponent {
+  /* @ngInject */
+  constructor($scope, $element) {
 
-function phonecatComponent() {
-  return {
-    scope: {},
-    bindToController: {
-      phones: "="
-    },
-    link: function(scope, elm, attrs, ctrl) {
-      ctrl.filterData = {
-        text: '',
-        orderBy: 'name'
-      }; //default search filter
-      ctrl.search = function(searchData) {
-        ctrl.filterData = searchData;
-        scope.$apply();
-      }
-    },
-    controller: function() {},
-    controllerAs: 'vm',
-    templateUrl: 'partials/phonecatComponent.tpl.html'
-  };
+  }
 }
+
+angular
+  .module('phonecatApp')
+  .directive('phonecatComponent', () => {
+  return {
+    templateUrl: 'partials/phonecatComponent.tpl.html',
+    controller: PhonecatComponent,
+    controllerAs: 'vm',
+    bindToController: true,
+    restrict: 'EA'
+  };
+});
